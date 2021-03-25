685439ef9692ff21c9d84a42a08fcd2dc6b5797b
zhangtao25 <wr.zhang25@outlook.com>
2021-03-26 00:29:59 +0800

    upd

---
+++ note.md
@@ -0,0 +1,20 @@
+两年经验面试阿里前端开发岗，已拿offer，这些知识点该放出来了 https://www.jianshu.com/p/4aba38d973c3
+vue常考点 https://www.jianshu.com/p/3507b078fe03
+
+策略：主攻vue2.0，还需要一个demo来准备面试题目
+
+const let var区别
+宏任务 微任务
+webpack多页面打包
+
+
+大方向：
+算法
+性能优化
+js基础
+
+浏览器兼容性
+
+响应式原理
+
+栅格布局原理
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React, { useEffect } from 'react'
+import Container from './layouts/Container'
+import './assets/init.css'
 
 const App = (props: any) =>{
-  return <div>123</div>
+  return (
+    <div>
+      <Container></Container>
+    </div>
+  )
 }
 
 export default App
